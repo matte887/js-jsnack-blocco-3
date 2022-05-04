d67dceb1e946ab5b05a591a1241b50805f6ae560
matte887 <m.erroi92@gmail.com>
2022-05-04 15:01:14 +0200

    Snack 1

---
+++ Snack 1/js/script.js
@@ -0,0 +1,25 @@
+// Crea due array che hanno un numero di elementi diversi. Aggiungi elementi all’array che ha meno elementi fino a quando ne avrà tanti quanti l’altro.
+
+const firstArray = [23, 52, 67];
+const secondArray = [42, 63, 34, 65, 43, 45];
+console.log("first array:", firstArray.length);
+console.log("second array:", secondArray.length);
+
+const flagVariable = secondArray.length > firstArray.length;
+if (flagVariable) {
+    while (firstArray.length < secondArray.length) {
+        const rndNumber = getRndInteger(1, 99);
+        firstArray.push(rndNumber)
+    }
+} else {
+    while (firstArray.length > secondArray.length) {
+        const rndNumber = getRndInteger(1, 99);
+        secondArray.push(rndNumber)
+    }
+}
+console.log("...first array:", firstArray);
+console.log("...second array:", secondArray);
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
